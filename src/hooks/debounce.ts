695874a6759599468812119118531307de4ce379
david schummer <david.schummer@ogs.de>
2026-07-08 15:00:34 +0200

    docs(debounce): add docs

--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -1,5 +1,42 @@
 import { useCallback, useEffect, useRef } from "react";
 
+/**
+ * React Hook that creates a debounced version of a callback.
+ *
+ * The returned callback delays execution until it has not been invoked
+ * again for the specified amount of time. If it is called repeatedly
+ * within the delay, only the latest invocation is executed.
+ *
+ * @example
+ * ```tsx
+ * function MyComponent() {
+ *   const debouncedSearch = useDebouncedCallback(
+ *     (value: string) => {
+ *       expensiveSearch(value);
+ *     },
+ *     300
+ *   );
+ *
+ *   return (
+ *     <input
+ *       onChange={(e) => debouncedSearch(e.target.value)}
+ *     />
+ *   );
+ * }
+ * ```
+ *
+ * @typeParam T - Tuple of argument types accepted by the callback.
+ *
+ * @param callback - Function to execute after the debounce delay.
+ * @param delay - Debounce delay in milliseconds.
+ *
+ * @returns A stable debounced callback. The latest invocation is executed
+ * after the configured delay.
+ *
+ * @category useDebouncedCallback
+ * @since 1.1.0
+ * @author David Schummer
+ */
 function useDebouncedCallback<T extends unknown[]>(
   callback: (...args: T) => void,
   delay: number
